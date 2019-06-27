86d1cd95133d514ca292476be5f60d8e6ac376ce
fzkangga <titahpriyangga@gmail.com>
2019-06-27 13:48:42 +0700

    trial sensor base codeaurora LA.BR.1.2.3_rb1.113

--- product/sensors.mk
+++ product/sensors.mk
@@ -1,3 +1,9 @@
+# Sensor HAL
+PRODUCT_PACKAGES += \
+    calmodule.cfg \
+    libcalmodule_common \
+    sensors.msm8909
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
