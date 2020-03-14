af69118b33d7b319369c222c5903015c17684917
fzkangga <titahpriyangga@gmail.com>
2020-03-14 17:37:46 +0700

    g151: fix separator

--- device.mk
+++ device.mk
@@ -88,7 +88,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.msm8909 \
     libmm-qcamera \
-    libshim_camera
+    libshim_camera \
     Snap
 
 PRODUCT_COPY_FILES += \
@@ -296,6 +296,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
-    $(LOCAL_PATH)/wifi/WCNSS_wlan_dictionary.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_wlan_dictionary.dat \
+    $(LOCAL_PATH)/wifi/WCNSS_wlan_dictionary.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_wlan_dictionary.dat
 
 include device/haier/g151/default-prop.mk
