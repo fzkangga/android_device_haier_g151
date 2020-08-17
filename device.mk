b0ab7cad784046fd0cde94e46135dc1776cefe94
fzkangga <titahpriyangga@gmail.com>
2020-08-17 12:00:35 +0700

    g151: update mixer paths
    fix audio for hy g151

--- device.mk
+++ device.mk
@@ -37,7 +37,14 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdb/QRD_Speaker_cal.acdb:system/etc/acdbdata/QRD/QRD_Speaker_cal.acdb \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/vendor/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/vendor/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/vendor/etc/mixer_paths_qrd_skui.xml
+    $(LOCAL_PATH)/audio/mixer_paths_qrd_skuh.xml:system/vendor/etc/mixer_paths_qrd_skuh.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_qrd_skui.xml:system/vendor/etc/mixer_paths_qrd_skui.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/vendor/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_msm8909_pm8916.xml:system/vendor/etc/mixer_paths_msm8909_pm8916.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_skua.xml:system/vendor/etc/mixer_paths_skua.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_skuc.xml:system/vendor/etc/mixer_paths_skuc.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_msm8909_x20_g151_pm8916.xml:system/vendor/etc/mixer_paths_msm8909_x20_g151_pm8916.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_skue.xml:system/vendor/etc/mixer_paths_skue.xml
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
