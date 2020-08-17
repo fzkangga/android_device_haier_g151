e3ff5fbdbe0efe6921c0d1eafae4a6c930cf3c5d
fzkangga <titahpriyangga@gmail.com>
2020-08-17 12:34:43 +0700

    g151: is g151

--- lineage.mk
+++ lineage.mk
@@ -13,25 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, device/wingtech/wt88047/full_wt88047.mk)
+$(call inherit-product, device/haier/g151/full_g151.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
-TARGET_BOARD_PLATFORM_VARIANT := msm8916
+TARGET_BOARD_PLATFORM_VARIANT := msm8909
 
-# Assert
-TARGET_OTA_ASSERT_DEVICE := HM2014811,HM2014812,HM2014813,HM2014814,HM2014815,HM2014816,HM2014817,HM2014818,HM2014819,HM2014820,HM2014821,HM2014112,wt88047,wt86047
+PRODUCT_NAME := lineage_g151
+BOARD_VENDOR := haier
+PRODUCT_DEVICE := g151
 
-PRODUCT_NAME := lineage_wt88047
-BOARD_VENDOR := wingtech
-PRODUCT_DEVICE := wt88047
+PRODUCT_GMS_CLIENTID_BASE := android-haier
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Build fingerprint
-BUILD_FINGERPRINT="Xiaomi/wt88047/wt88047:5.1.1/LMY47V/V9.2.5.0.LHJMIEK:user/release-keys"
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="wt88047-user 5.1.1 LMY47V V9.2.5.0.LHJMIEK release-keys"
+TARGET_VENDOR_PRODUCT_NAME := A16C3H
+TARGET_VENDOR_DEVICE_NAME := A16C3H
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=A16C3H PRODUCT_NAME=A16C3H
